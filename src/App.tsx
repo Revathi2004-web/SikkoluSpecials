3e357cfcb7d17cd55dee05f6e60fb29efb849854
Revathi2004-web <chintalarevathi2@gmail.com>
2026-02-03 18:31:18 +0530

    Update routing to use Index and Checkout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import { AdminDashboard } from './pages/AdminDashboard';
-import { TrackOrder } from './pages/TrackOrder';
-import { MyOrders } from './pages/MyOrders';
-import CheckoutPage from './pages/checkout';
+import Index from './pages/Index'; 
+import AdminDashboard from './pages/AdminDashboard';
+import TrackOrder from './pages/TrackOrder';
+import MyOrders from './pages/MyOrders';
+import Checkout from './pages/checkout'; 
 import './index.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* Main Home Page (Index) */}
+        <Route path="/" element={<Index />} />
+        
+        {/* Admin Page */}
         <Route path="/admin" element={<AdminDashboard />} />
+        
+        {/* Other Pages */}
         <Route path="/track-order" element={<TrackOrder />} />
         <Route path="/my-orders" element={<MyOrders />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="*" element={<HomePage />} />
+        <Route path="/checkout" element={<Checkout />} />
+        
+        {/* Default redirect to Home */}
+        <Route path="*" element={<Index />} />
       </Routes>
     </BrowserRouter>
   );
